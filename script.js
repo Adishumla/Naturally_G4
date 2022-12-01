a158b355f53fc6136d3350a386152db32cf899dc
Adishumla <45212964+Adishumla@users.noreply.github.com>
2022-12-01 15:25:06 +0100

    font fixed

--- script.js
+++ script.js
@@ -204,3 +204,31 @@ heartContainers.forEach((heartContainer) => {
   });
 });
  */
+
+/* set active-img on all img when they are in viewport on mobile */
+const img = document.querySelectorAll(".img");
+
+const isInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const run = () =>
+  img.forEach((el) => {
+    if (isInViewport(el)) {
+      el.classList.add("active");
+    } else {
+      el.classList.remove("active");
+    }
+  });
+
+// Events
+window.addEventListener("load", run);
+window.addEventListener("resize", run);
+window.addEventListener("scroll", run);
